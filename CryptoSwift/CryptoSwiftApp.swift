b7f6c9cdf363acf35740c48172f7716ade2e93bd
swaruppanda11 <swaruppanda11@gmail.com>
2025-12-30 22:09:34 -0800

    Initial Commit

---
+++ CryptoSwift/CryptoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftApp.swift
+//  CryptoSwift
+//
+//  Created by Swarup Panda on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
